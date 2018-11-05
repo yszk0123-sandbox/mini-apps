0aab6cdaba2f546ff1ef0e692daffad60fbe508a
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-11-06 01:50:35 +0900

    feat(time): update design

--- pages/time.js
+++ pages/time.js
@@ -1,33 +1,63 @@
+import { useEffect, useState } from "react";
 import { format } from "date-fns";
 import styled from "styled-components";
 import { GlobalStyle } from "../components/GlobalStyle";
-import { fontSizeLarge } from "../lib/constants/StyleConstants";
+import {
+  fontSizeLarge,
+  bgColorDefault,
+  fgColorDefault
+} from "../lib/constants/StyleConstants";
 
 const Screen = styled.div`
-  padding: 0;
   align-items: center;
   display: flex;
   height: 100vh;
   justify-content: center;
+  padding: 0;
   width: 100vw;
 `;
 
 const Box = styled.div`
   align-items: center;
+  background: ${bgColorDefault};
+  color: ${fgColorDefault};
   display: flex;
-  justify-content: center;
   font-size: ${fontSizeLarge};
   font-weight: bold;
-  width: 80%;
   height: 40%;
-  background: lightgray;
+  justify-content: center;
+  width: 80%;
 `;
 
-const Time = () => (
-  <Screen>
-    <Box>Time: {format(new Date(), "YYYY/MM/DD hh:mm:ss")}</Box>
-    <GlobalStyle />
-  </Screen>
-);
+const INTERVAL = 1000;
+
+const useCurrentDate = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setDate(new Date());
+    }, INTERVAL);
+
+    return () => {
+      if (timerId) {
+        clearInterval(timerId);
+      }
+    };
+  });
+
+  return date;
+};
+
+const Time = () => {
+  const date = useCurrentDate();
+
+  return (
+    <Screen>
+      <Box>{format(date, "YYYY/MM/DD hh:mm:ss")}</Box>
+      <GlobalStyle />
+    </Screen>
+  );
+};
 
 export default Time;
